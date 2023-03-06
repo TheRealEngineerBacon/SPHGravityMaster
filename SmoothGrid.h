0bdbd6c1ef8c4971e45589580579a09e4445c657
TheRealEngineerBacon <Engineer_Bacon@yahoo.com>
2023-03-05 21:33:18 -0700

    cleanup

--- SmoothGrid.h
+++ SmoothGrid.h
@@ -1,31 +1,30 @@
 #ifndef SMOOTHGRID_H
 #define SMOOTHGRID_H
+
 #include "particle.h"
 #include <vector>
 #include <array>
-
 
 class SmoothGrid {
 public:
 	std::vector<std::vector<std::vector<std::vector<int>>>> grid;
 
-	SmoothGrid(int size, long double h) {
+	SmoothGrid(int size, long double h, int n) {
 		grid.resize(size, std::vector<std::vector<std::vector<int>>>
 				   (size, std::vector<std::vector<int>>
 				   (size, std::vector<int>())));
 	}
 
-	void on_grid(Particle** array, int n, int grid_size, long double h, std::array<long double, 6> center_pos) {
+	void update_grid(Particle** array, const int n, const int grid_size, const long double h, const std::array<long double, 6> center_pos) {
 		long double h_adj = calculate_h(grid_size, h, center_pos);
 		for (int i = 0; i < n; ++i) {
 			if ((*array[i]).on_grid == true) {
-				int offset = floor(grid_size / 2.f);
-				int a{}, b{}, c{};
-				long double x{ (*array[i]).x }, y{ (*array[i]).y }, z{ (*array[i]).z };
-				long double h_half = h_adj / 2;
-				a = floor((x + h_half - center_pos[0]) / h_adj) + offset;
-				b = floor((y + h_half - center_pos[1]) / h_adj) + offset;
-				c = floor((z + h_half - center_pos[2]) / h_adj) + offset;
+				const int offset = (int)floor(grid_size / 2.f);
+				const long double x{ (*array[i]).x }, y{ (*array[i]).y }, z{ (*array[i]).z };
+				const long double h_half = h_adj / 2;
+				const int a = (int)floor((x + h_half - center_pos[0]) / h_adj) + offset;
+				const int b = (int)floor((y + h_half - center_pos[1]) / h_adj) + offset;
+				const int c = (int)floor((z + h_half - center_pos[2]) / h_adj) + offset;
 				if ((*array[i]).ind_x != a || (*array[i]).ind_y != b || (*array[i]).ind_z != c) {
 					int ind_x = (*array[i]).ind_x;
 					int ind_y = (*array[i]).ind_y;
@@ -46,21 +45,20 @@ public:
 		}
 	}
 
-	void place(Particle** array, int i, int grid_size, long double h, std::array<long double, 6> center_pos, long double h_adj) {
-		int offset = floor(grid_size / 2.f);
-		int a{}, b{}, c{};
-		long double x{ (*array[i]).x }, y{ (*array[i]).y }, z{ (*array[i]).z };
-		long double h_half = h_adj / 2;
-		a = floor((x + h_half - center_pos[0]) / h_adj) + offset;
-		b = floor((y + h_half - center_pos[1]) / h_adj) + offset;
-		c = floor((z + h_half - center_pos[2]) / h_adj) + offset;
+	void place(Particle** array, const int i, const int grid_size, const long double h, const std::array<long double, 6> center_pos, const long double h_adj) {
+		const int offset = (int)floor(grid_size / 2.f);
+		const long double x{ (*array[i]).x }, y{ (*array[i]).y }, z{ (*array[i]).z };
+		const long double h_half = h_adj / 2;
+		const int a = (int)floor((x + h_half - center_pos[0]) / h_adj) + offset;
+		const int b = (int)floor((y + h_half - center_pos[1]) / h_adj) + offset;
+		const int c = (int)floor((z + h_half - center_pos[2]) / h_adj) + offset;
 		grid[a][b][c].push_back((*array[i]).id);
 		(*array[i]).ind_x = a;
 		(*array[i]).ind_y = b;
 		(*array[i]).ind_z = c;
 	}
 
-	int find_element(int id, int ind_x, int ind_y, int ind_z) {
+	int find_element(const int id, const int ind_x, const int ind_y, const int ind_z) {
 		for (int i = 0; i < grid[ind_x][ind_y][ind_z].size(); ++i) {
 			if (grid[ind_x][ind_y][ind_z][i] == id) {
 				return i;
@@ -68,32 +66,11 @@ public:
 		}
 	}
 
-	long double calculate_h(int grid_size, long double h, std::array<long double, 6> center_pos) {
-		long double max_element = *std::max_element(center_pos.begin() + 3, center_pos.end());
-		int adj_value = (max_element * 2) / grid_size;
-		return std::fmaxl(adj_value, h);
-	}
-
-	std::vector<int> return_surr(Particle** array, int focus, int grid_size) {
-		std::vector<int> temp;
-		int sindex_x{ (*array[focus]).ind_x }, sindex_y{ (*array[focus]).ind_y }, sindex_z{ (*array[focus]).ind_z };
-		for (int i = sindex_x - 1; i < (sindex_x + 2); ++i) {
-			if (i < 0 || i > grid_size - 1)
-				continue;
-			for (int j = sindex_y - 1; j < (sindex_y + 2); ++j) {
-				if (j < 0 || j > grid_size - 1)
-					continue;
-				for (int k = sindex_z - 1; k < (sindex_z + 2); ++k) {
-					if (k < 0 || k > grid_size - 1)
-						continue;
-					for (int p = 0; p < grid[i][j][k].size(); ++p)
-						temp.push_back(grid[i][j][k][p]);
-				}
-			}
-		}
-		return temp;
+	long double calculate_h(const int grid_size, const long double h, const std::array<long double, 6> center_pos) {
+		const long double element = *(std::max_element(center_pos.begin() + 3, center_pos.end()));
+		const long double adj_value = (element * 2) / grid_size;
+		return std::fmaxl(adj_value, 1.5 * h);
 	}
 };
-
 
 #endif
--- constants.h
+++ constants.h
@@ -7,13 +7,13 @@
 
 //Simulation Constants
 constexpr int n{ 256 };
-constexpr int compute_ratio = 200;
+constexpr int compute_ratio = 100;
 std::array<long double, 6> center_pos{};
 float delta_t = 0.5;
 short tick{};
 bool friction{ false };
 constexpr long double h = 1e6;
-int grid_size = 13;
+const int grid_size = 19;
 
 
 //Render Constants
@@ -22,6 +22,6 @@ float alpha{}, beta{}, gamma{}, scale{};
 int focus{};
 float center_mass{}, x_mov, y_mov;
 float display_radius{ 1e8 };
-bool pause_state = false;
+bool pause_state = true;
 
 #endif
